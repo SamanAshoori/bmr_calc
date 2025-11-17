019109eda98422c7ed430ccf8e007d82d76d939e
Saman Ashoori <saman@samanashoori.com>
2025-11-17 20:26:57 +0000

    aks user age

--- mifflin-st_jeor_equation.cpp
+++ mifflin-st_jeor_equation.cpp
@@ -3,10 +3,14 @@
 int main()
 {
     std::string name;
+    int age;
     std::cout << "What's your name?: ";
     std::cin >> name;
 
     std::cout << "Hello " << name;
+
+    std::cout << "What's your age: ";
+    std::cin >> age;
 
     return 0;
 }
