109473a412055dfc2f8420c13f680214bac8a70a
Saman Ashoori <saman@samanashoori.com>
2025-11-17 20:29:04 +0000

    created variables

--- mifflin-st_jeor_equation.cpp
+++ mifflin-st_jeor_equation.cpp
@@ -4,10 +4,14 @@ int main()
 {
     std::string name;
     int age;
+    int height;
+    int weight;
+    int activity_factor;
+
     std::cout << "What's your name?: ";
     std::cin >> name;
 
-    std::cout << "Hello " << name;
+    std::cout << "Hello " << name << '\n';
 
     std::cout << "What's your age: ";
     std::cin >> age;
